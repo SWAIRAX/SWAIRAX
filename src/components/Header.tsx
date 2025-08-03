06a7914248961ba9b195155de9131dd266684270
SWAY <www.florencesway@gmail.com>
2025-08-03 18:16:47 +0300

    changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -48,6 +48,7 @@ const Header = () => {
       path: "#",
       dropdown: [
         { name: "Comming Soon", desc: "Comprehensive guides and API docs", path: "#" },
+        {name: "Research", desc: "Explore Quantum Intelligence’s evolving journey through innovation.", path: "/Research"},
       //   { name: "Whitepapers", desc: "In-depth research and industry insights", path: "/resources" },
       //   { name: "Video Tutorials", desc: "Step-by-step guides and webinars", path: "/resources" }
       ]
